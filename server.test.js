59e29a28a42c9c8a04414420ecc28450022e4137
JV Madic <john16_madic@yahoo.com>
2021-08-13 10:49:22 +1200

    Create test for home page

---
+++ server.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+
+const server = require('./server')
+
+test('GET / returns correct response', (done) => {
+    request(server)
+    .get('/') // get home page
+    .expect(200) //response code
+    .end((err, res) => {
+        expect(err).toBeNull() //expect no error
+        expect(res.text).toMatch() // to match whatever content is written on the home page
+        done()
+    }) 
+})
