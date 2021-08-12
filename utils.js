933be163e8a9d00d0f0e8ebd7d41e84a2a7a6eb2
Siyang <csiyang@yahoo.co.nz>
2021-08-13 11:37:22 +1200

    created getData function

--- utils.js
+++ utils.js
@@ -1,4 +1,25 @@
-const fs = required('fs')
-const path = required('path')
+const fs = require('fs')
+const path = require('path')
 
-module.exports = {}
+module.exports = { getData }
+
+function getData(cb) {
+  const filePath = path.join(__dirname, 'data.json')
+
+  fs.readFile(filePath, 'utf8', (err, content) => {
+    if (err) {
+      console.error(err.message)
+      cb(new Error ("I got lost trying to find the file"))
+      return
+    }
+    try {
+      parseData = JSON.parse(content)
+      cb(null, parseData)
+    } catch (parseError) {
+      console.error(parseError.message)
+      cb(new Error ("Wow reading that data was too hard for me!"))
+
+    }
+  
+  })
+}
