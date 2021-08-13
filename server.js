bae3398f48bf6ae18ecd0262d657987af8dc9f1b
Dianne Plaza <d.plaza29@me.com>
2021-08-13 15:21:11 +1200

    fix random function to randomise message

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const {getData} = require('./utils')
+const { getData } = require('./utils')
 const server = express()
 const blobFish = require('./routes')
 const hbs = require('express-handlebars')
-
 
 // Server configuration
 server.use(express.static('public'))
@@ -16,49 +15,49 @@ server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 server.get('/:page', (req, res) => {
-    const searchPage = Number(req.params.page)
-    let msg = ""
-    getData((err, data) => {
-        if(err) {
-            res.status(500).send(err.message)
-            return
-        }
-        let randomNumber = Math.floor(Math.random())
-        if(searchPage === 1) {
-            const messageRandomNumber = randomNumber * (data.allMessage[0].message.length-1)
-            msg = data.allMessage[0].message[messageRandomNumber]
-        } else if(searchPage === 2) {
-            const messageRandomNumber = randomNumber * (data.allMessage[1].message.length-1)
-            msg = data.allMessage[1].message[messageRandomNumber]
-        } else if(searchPage === 3) {
-            const messageRandomNumber = randomNumber * (data.allMessage[2].message.length-1)
-            msg = data.allMessage[2].message[messageRandomNumber]
-        }
-        const viewData = {
-          msg
-        }
-        res.render('message', viewData)
-    })
+  const searchPage = Number(req.params.page)
+  let msg = ''
+  getData((err, data) => {
+    if (err) {
+      res.status(500).send(err.message)
+      return
+    }
+    const randomNumber = Math.floor(Math.random() * 3)
+    if (searchPage === 1) {
+      const messageRandomNumber = randomRange(0, 4)
+      console.log(messageRandomNumber)
+      msg = data.allMessage[0].message[messageRandomNumber]
+    } else if (searchPage === 2) {
+      const messageRandomNumber = randomRange(0, 4)
+      msg = data.allMessage[1].message[messageRandomNumber]
+    } else if (searchPage === 3) {
+      const messageRandomNumber = randomRange(0, 4)
+      msg = data.allMessage[2].message[messageRandomNumber]
+    }
+    const viewData = {
+      msg
+    }
+    res.render('message', viewData)
+  })
 })
-
 
 server.get('/', (req, res) => {
-    getData((err, data) => {
-        if(err) {
-            res.status(500).send('getting data Error')
-            return
-        }
+  getData((err, data) => {
+    if (err) {
+      res.status(500).send('getting data Error')
+      return
+    }
 
-        const viewData = {
-          data
-        }
+    const viewData = {
+      data
+    }
 
-        res.render('home', viewData)
-
-    })
+    res.render('home', viewData)
+  })
 })
 
-
-
-
 module.exports = server
+
+function randomRange (min, max) {
+  return Math.floor(Math.random() * (max - min) + 1 + min)
+}
